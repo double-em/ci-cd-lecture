6c9bc975830052514b3d055c744ae232dd98c1d7
double-em <meldgaardmike@gmail.com>
2021-11-17 18:18:41 +0100

    Removed comments

--- MyApi/Startup.cs
+++ MyApi/Startup.cs
@@ -23,14 +23,12 @@ namespace MyApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "MyApi", Version = "v1"}); });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
